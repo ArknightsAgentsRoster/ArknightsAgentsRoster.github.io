864f28ced4521d8e25f241262c797e4f68fa93c8
ArknightsAgentsRoster <wilsonlo1997@gmail.com>
2019-08-30 17:43:00 +0800

    Version: 1.02

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e22915fa0049585e8e56de3d29584ca.js"
+  "/precache-manifest.902953fe5636038f94c8abd39cc66b5c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "arknight"});
